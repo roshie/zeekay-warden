375672e2d4bc33ccdd7b110b312191d410914f8f
JoekinRoche <joekinroches@gmail.com>
2024-09-21 00:06:36 +0530

    Added airdao testnet

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config()
 // Ensure your configuration variables are set before executing the script
 const { vars } = require("hardhat/config");
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+// const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -13,17 +13,22 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+// const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
-
+// airdao
 module.exports = {
   solidity: {
     version: "0.8.24"
   },
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
+    airdao: {
+      url: `https://network.ambrosus-test.io/`,
+      accounts: [process.env.AIRDAO_PRIVATE_KEY], 
+      chainId: 22040, 
     },
   },
   paths: {
