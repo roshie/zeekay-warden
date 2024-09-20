067dc08e1597e7c1d98b343b4202331d7f1114e6
roshie <roshithaignatius21@gmail.com>
2024-09-21 01:50:09 +0800

    add user function

--- contracts/src/ZeekayWarden.sol
+++ contracts/src/ZeekayWarden.sol
@@ -8,12 +8,39 @@ interface IPlonkVerifier {
 
 contract ZeekayWarden {
     address public s_plonkVerifierAddress;
+    event MemberVerified(bool result);
+
+    // Member address => roles
+    mapping(address => mapping(string => Membership)) public members;
+
+    struct Membership {
+        string[] roles;
+        string platformUID;
+    }
+
+    constructor(address plonkVerifierAddress) 
+    {
+        s_plonkVerifierAddress = plonkVerifierAddress;
+    }
 
     // ZK proof is generated in the browser and submitted as a transaction w/ the proof as bytes.
     function submitProofAndAddUser(bytes memory proof, uint256[] memory pubSignals, string[] memory data, string[] memory roles) public returns (bool) {
         bool result = IPlonkVerifier(s_plonkVerifierAddress).verifyProof(proof, pubSignals);
 
+        if (result) {
+            addUser(data, roles);
+        }
+        // Emit event
+        emit MemberVerified(result);
         return result;
+    }
+
+    function addUser(string[] memory data, string[] memory roles) private {
+        address user = msg.sender;
+        string memory serverId = data[0];
+        string memory platformUID = data[1];
+        Membership memory member = Membership({platformUID: platformUID, roles: roles});
+        members[user][serverId] = member;
     }
 
 
